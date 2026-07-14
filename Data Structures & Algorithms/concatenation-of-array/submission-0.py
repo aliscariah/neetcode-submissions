96796500d5dfd41a6b8392c524d2d5a917c5fc8e
aliscariah <116733249+aliscariah@users.noreply.github.com>
2026-07-14 23:57:16 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+        ans=[]
+        for i in range(0,n):
+            ans.append(nums[i])
+        for i in range(0,n):
+            ans.append(nums[i])
+            
+            
+        return ans
+
+
